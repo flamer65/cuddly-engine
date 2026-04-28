1eb3d1a0c663221db81d7f0f9d8a686c7833f672
flamer <namank6510@gmail.com>
2026-04-28 15:42:54 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,35 +2,39 @@ import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
-function parseRESP(data: string): string[] {
-  const lines = data.split("\r\n");
-  const args: string[] = [];
-
-  
-  for(let i = 0; i < lines.length; i++){
-    if(lines[i].startsWith("$")){
-        args.push(lines[i+1]);
-        i++;
-    }
-  }
-  return args;
+const commandsParser = (data: Buffer) => {
+ return data.toString().split("\r\n").filter((c) => !c.startsWith("*") && !c.startsWith("$") && c !== "");
 }
+const writeRESPSimpleString = (data: string):string =>{
+     return `+${data}\r\n`;
+}
+const writeRESPBulkString = (data: string):string =>{
+     return `$${data.length}\r\n${data}\r\n`;
+}
+const mem = new Map<String, any>();
 // Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  connection.on("data", (data) => {
-    const args = parseRESP(data.toString());
-      const command = args[0].toUpperCase();
-    switch (command) {
+  connection.on("data", (data: Buffer) => {
+    const args = commandsParser(data);
+    const command = args[0];
+    const value = args[1];
+    switch (command.toUpperCase()) {
       case "PING":
-        connection.write("+PONG\r\n");
+        connection.write(writeRESPSimpleString("PONG"));
         break;
       case "ECHO":
-        const msg = args[1];
-        connection.write(`$${msg.length}\r\n${msg}\r\n`);
+        connection.write(writeRESPBulkString(value));
+        break;
+      case "SET":
+        mem.set(value, args[2]);
+        connection.write(writeRESPSimpleString("OK"));
+        break;
+      case "GET":
+        connection.write(writeRESPBulkString(mem.get(value) || ""));
         break;
       default:
         connection.write("-ERR unknown command\r\n");
+        break;
     }
   });
   // Handle connection
