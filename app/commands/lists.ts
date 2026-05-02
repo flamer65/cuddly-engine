5e2aa729de2355bf53033e828827f8159292305f
flamer <namank6510@gmail.com>
2026-05-02 12:53:43 +0530

    codecrafters submit [skip ci]

--- app/commands/lists.ts
+++ app/commands/lists.ts
@@ -1,61 +1,61 @@
 import { listStore } from "../store";
-import { integer, nullBulk, respArray,writeRESPBulkString } from "../resp";
+import { integer, nullBulk, respArray, writeRESPBulkString } from "../resp";
 import * as net from "net";
 
-export function handleRpush(args: string[], connection: net.Socket){
-    const key = args[1];
-    if(!listStore.has(key)) listStore.set(key, [])
-    const list = listStore.get(key)!;
-    for (let i = 2; i < args.length; i++) {
+export function handleRpush(args: string[], connection: net.Socket) {
+  const key = args[1];
+  if (!listStore.has(key)) listStore.set(key, []);
+  const list = listStore.get(key)!;
+  for (let i = 2; i < args.length; i++) {
     list.push(args[i]);
   }
   connection.write(integer(list.length));
 }
 
-export function handleLpush(args: string[], connection: net.Socket){
-    const key = args[1];
-    if(!listStore.has(key)) listStore.set(key, [])
-    const list = listStore.get(key)!;
-    for (let i = 2; i < args.length; i++) {
+export function handleLpush(args: string[], connection: net.Socket) {
+  const key = args[1];
+  if (!listStore.has(key)) listStore.set(key, []);
+  const list = listStore.get(key)!;
+  for (let i = 2; i < args.length; i++) {
     list.unshift(args[i]);
   }
   connection.write(integer(list.length));
 }
-export function handleLrange(args: string[], connection: net.Socket){
-    const key = args[1];
-    const list = listStore.get(key) || [];
-   
-    let start = parseInt(args[2]);
-    let end = parseInt(args[3]);
-    if(start < 0) start = Math.max(list.length + start, 0);
-    if(end < 0) end = list.length + end;
+export function handleLrange(args: string[], connection: net.Socket) {
+  const key = args[1];
+  const list = listStore.get(key) || [];
 
-    const slice = list.slice(start, end + 1);
-    connection.write(respArray(slice));
+  let start = parseInt(args[2]);
+  let end = parseInt(args[3]);
+  if (start < 0) start = Math.max(list.length + start, 0);
+  if (end < 0) end = list.length + end;
+
+  const slice = list.slice(start, end + 1);
+  connection.write(respArray(slice));
 }
 
-export function handleLlen(args: string[], connection: net.Socket){
-    const key = args[1];
-    const list = listStore.get(key) || [];
-    connection.write(integer(list.length));
+export function handleLlen(args: string[], connection: net.Socket) {
+  const key = args[1];
+  const list = listStore.get(key) || [];
+  connection.write(integer(list.length));
 }
 
-export function handleLpop(args: string[], connection: net.Socket){
-    const key = args[1];
-    const count = parseInt(args[2]) || 1;
-    const list = listStore.get(key) || [];
-    const res:string[] = []
-    if(count > 1 && count > list.length){
-        for (let i = 0; i < count && list.length > 0; i++) {
-            res.push(list.shift()!);
-        }
-        connection.write(respArray(res));
-    }else{
-       const value = list.shift();
-       if(value){
-        connection.write(writeRESPBulkString(value));
-    }else{
-        connection.write(nullBulk());
+export function handleLpop(args: string[], connection: net.Socket) {
+  const key = args[1];
+  const count = parseInt(args[2]) || 1;
+  const list = listStore.get(key) || [];
+  const res: string[] = [];
+  if (count > 1 || count > list.length) {
+    for (let i = 0; i < count && list.length > 0; i++) {
+      res.push(list.shift()!);
     }
+    connection.write(respArray(res));
+  } else {
+    const value = list.shift();
+    if (value) {
+      connection.write(writeRESPBulkString(value));
+    } else {
+      connection.write(nullBulk());
     }
+  }
 }
